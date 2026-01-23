87bb768ce6f01e0f933a53ea73465eb961e7d4dc
cearango00 <cearango@latam.stefanini.com>
2026-01-23 12:01:04 -0500

    Delete management directory
    se borra la carpeta managment

--- management/src/main/java/com/card/management/Exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.card.management.Exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-  public UserNotFoundException(String message) {
-    super(message);
-  }
-}
